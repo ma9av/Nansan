49fcea846f466d5130ed95e8241c783bbc4c1380
ma9av <manavpanchal42@gmail.com>
2024-01-11 03:04:56 +0530

    change

--- my-app/components/Clients.js
+++ my-app/components/Clients.js
@@ -5,10 +5,10 @@ const Clients = () => {
         <h1 className=" text-center mt-10"><span className='bg-blue-950 p-2 text-white rounded-2xl'>  Our Clients </span></h1>
 
         <div className='pt-10'>
-        <div className='flex p-5 justify-center pt-5'>
-            <Image src={"/clients/edutest.png"} width={200} height={200} alt='' className='p-10'/>
-            <Image src={"/clients/NTA.png"} width={300} height={200} alt='' className='p-10'/>
-            <Image src={"/clients/asquare.webp"} width={200} height={50} alt='' className='p-10'/>
+        <div className='flex  justify-center pt-5 md:flex-row md:p-10 horizontal'>
+            <Image src={"/clients/edutest.png"} width={200} height={200} alt='' className='p-5'/>
+            <Image src={"/clients/NTA.png"} width={300} height={200} alt='' className='p-5'/>
+            <Image src={"/clients/asquare.webp"} width={200} height={50} alt='' className='p-5'/>
         </div>
         <div className='flex justify-center gap-40 pt-5 p-10'>
 
